17c59e70307384d20af881449939e20b12f780e1
Sungmin Lee <lsm0729@bos-semi.com>
2024-09-26 16:25:03 +0900

    .

---
+++ build.sh
@@ -0,0 +1,8 @@
+sudo apt-get install gcc g++ gdb -y
+sudo apt-get install make cmake -y
+sudo apt-get install -y 
+sudo apt-get update && \
+    apt-get install -y python3 python3-pip
+sudo apt-get install -y lsb-release wget software-properties-common gnupg clang lld
+sudo apt-get install -y ccache
+sudo apt-get install ninja-build
